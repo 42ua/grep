48b1c50a7372b59e4d879380afae138fd146333f
Oleg Mazko <o.mazko@mail.ru>
2022-12-19 20:31:43 +0200

    bitbucket snippets

--- assets/libs/main.js
+++ assets/libs/main.js
@@ -75,26 +75,27 @@ $(function() {
 
   (function() {
     $("li a.gist-api").click(function() {
+      var formData = new FormData();
+      formData.append("file",  new File([$("#grep-stdin").val()], 'stdin', {type: 'text/plain'}));
+      formData.append("file",  new File([$("#grep-stdout").val()], 'stdout', {type: 'text/plain'}));
+      formData.append("file",  new File([$("#grep-cmd").val()], 'cmd', {type: 'text/plain'}));
+      formData.append('title', 'grep.js');
       $.ajax({
+        cache: false,
+        contentType: false,
+        processData: false,
         type: "POST",
-        url: 'https://api.github.com/gists',
+        url: 'https://api.bitbucket.org/2.0/snippets/grepjs',
         headers: {
-          "Authorization": 'Basic bWF6a29ib3Q6MWY1ZjAxODgzNTQzMjU4Yjc5NjdjNmYzODcxZmI0NDIzZDRiM2E0MA==',
+          'Authorization': 'Basic bWF6a29ib3Q6QVRCQnNRblEzejU0Qks1QUJlUXQ0ejJYVU5hRzIxNjhBNTlC'
         },
-        data: JSON.stringify({
-          "description": "grep.js",
-          "files": {
-            "stdin": {"content": $("#grep-stdin").val()},
-            "stdout": {"content": $("#grep-stdout").text()},
-            "args": {"content": $("#grep-cmd").val()}
-          }
-        })
+        data: formData,
       }).done(function(response) {
-        var url = response.html_url,
-            my = $(location).attr('href').replace(/(#|\?).*$/, "") + '?gist=' + response.id;
+        var url = response.links.html.href,
+            my = $(location).attr('href').replace(/(#|\?).*$/, "") + '?snippet=' + response.id;
         $(".user-errors-here").append( "<div class='alert alert-success alert-dismissible fade in' role=alert>" + 
           "<button type=button class=close data-dismiss=alert aria-label=Close><span aria-hidden=true>&times;</span></button>" + 
-          "<strong>GIST:</strong> <a href='" + url + "'>" + response.id + "</a> | " + 
+          "<strong>Snippet:</strong> <a href='" + url + "'>" + response.id + "</a> | " + 
           "<strong>Share:</strong> <a href='" + my + "'>me</a>" + 
           "</div>"
         );
@@ -112,43 +113,48 @@ $(function() {
 /* Gist load or default */
 
 (function() {
-  function getParameterByName(name, url) {
-      if (!url) url = window.location.href;
-      name = name.replace(/[\[\]]/g, "\\$&");
-      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-          results = regex.exec(url);
-      if (!results) return null;
-      if (!results[2]) return '';
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
-
-  var gistId = getParameterByName('gist') || 'c077861c4e2f4095498aab9b0fd2ced0',
-      stdinGist = getParameterByName('stdin') || 'stdin',
-      argsGist = getParameterByName('args') || 'args',
+  var params    = new URLSearchParams(window.location.search),
+      gistId    = params.get('gist'),
+      snippetId = params.get('snippet') || 'q7KKKG',
       doc_ready = $.Deferred();
 
   /* http://stackoverflow.com/q/10326398 */
 
   $(doc_ready.resolve);
 
-  $.when( 
-    $.get( 'https://api.github.com/gists/' + gistId),
-    doc_ready )
-  .then(function( data ) {
-    var args = data[0].files[argsGist].content, 
-        rows = args.split(/\r\n|\r|\n/).length,
-        stdin = data[0].files[stdinGist].content;
-    $("#grep-cmd").val(args).attr("rows", rows).css({"height": rows > 1 ? "auto" : "34px"});
-    // document.ready() callbacks are called in the order they were registered. 
-    // If you register your testing callback first, it will be called first
-    // keyup() listener is registered earlier in this file
-    $("#grep-stdin").val(stdin).keyup();
-  })
-  .fail(function( e ) {
-    $(".user-errors-here").append( "<div class='alert alert-danger alert-dismissible fade in' role=alert>" + 
-      "<button type=button class=close data-dismiss=alert aria-label=Close><span aria-hidden=true>&times;</span></button>" + 
-      "<strong>Holy guacamole!</strong> " + [e.status, e.statusText] + "</div>"
-    );
-  });
+  if (gistId) {
+    var content = $.when(
+      $.get( 'https://api.github.com/gists/' + gistId),
+      doc_ready )
+    .then(function( data ) {
+      try { return { args: data[0].files.args.content, stdin: data[0].files.stdin.content }; }
+      catch(e) { return $.Deferred().reject({ statusText: 'invalid gist format', status: -1 }); }
+    });
+  } else {
+    var content = $.when(
+      $.get( 'https://api.bitbucket.org/2.0/snippets/grepjs/' + snippetId + '/files/cmd'),
+      $.get( 'https://api.bitbucket.org/2.0/snippets/grepjs/' + snippetId + '/files/stdin'),
+      doc_ready )
+    .then(function( data_args, data_stdin ) {
+      return { args: data_args[0], stdin: data_stdin[0] };
+    });
+  }
+
+  content.then(function( data ) {
+      var args  = data.args,
+          rows  = args.split(/\r\n|\r|\n/).length,
+          stdin = data.stdin;
+      $("#grep-cmd").val(args).attr("rows", rows).css({"height": rows > 1 ? "auto" : "34px"});
+      // document.ready() callbacks are called in the order they were registered.
+      // If you register your testing callback first, it will be called first
+      // keyup() listener is registered earlier in this file
+      $("#grep-stdin").val(stdin).keyup();
+    })
+    .fail(function( e ) {
+      $(".user-errors-here").append( "<div class='alert alert-danger alert-dismissible fade in' role=alert>" +
+        "<button type=button class=close data-dismiss=alert aria-label=Close><span aria-hidden=true>&times;</span></button>" +
+        "<strong>Holy guacamole!</strong> " + [e.status, e.statusText] + "</div>"
+      );
+    });
 
 })();
